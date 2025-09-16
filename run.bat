0761c65d3807b9b379e583b061818bf9343b6952
gisellavd2 <163237016+gisellavd2@users.noreply.github.com>
2025-09-16 11:31:59 +0700

    [update] facility image di tablet size

--- run.bat
+++ run.bat
@@ -1,3 +1,6 @@
 @echo off
 cd /d "C:\laragon\www\interactive_screen"
-"C:\laragon\bin\php\php-8.2.25-nts-Win32-vs16-x64\php.exe" artisan serve
+start "" "C:\laragon\bin\php\php-8.2.25-nts-Win32-vs16-x64\php.exe" artisan serve
+timeout /t 2
+taskkill /F /IM chrome.exe /T
+start "" chrome --new-window --start-fullscreen "http://127.0.0.1:8000"
